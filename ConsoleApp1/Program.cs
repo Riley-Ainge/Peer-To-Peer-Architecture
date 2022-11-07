b70e9d06ab3ddf2b1dd52a5563df8c6c213eff38
Riley-Ainge <117593905+Riley-Ainge@users.noreply.github.com>
2022-11-07 15:38:53 +0800

    Add files via upload

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using RemotingService;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ServiceHost host;
+            NetTcpBinding tcp = new NetTcpBinding();
+            host = new ServiceHost(typeof(Remoting));
+            host.AddServiceEndpoint(typeof(RemotingInterface), tcp, "net.tcp://" + "localhost" + ":" + "8200" + "/RemotingService");
+            host.Open();
+            Console.ReadLine();
+        }
+    }
+}
